4cfb82ee37c6dc5fe41f5318583f604550994468
Glitch (glitch-hello-node) <none>
2022-07-30 13:49:09 +0000

    🚠👷 Checkpoint ./server.js:4966520/5086 ./package.json:4966520/4637 ./LICENSE.md:4966520/13905 ./README.md:4966520/8558

--- server.js
+++ server.js
@@ -1,120 +1,45 @@
-/**
- * This is the main Node.js server script for your project
- * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
- */
+/* Setting things up. */
+const express = require( 'express' ),
+      app = express(),
+      CronJob = require( 'cron' ).CronJob,
+      Twit = require( 'twit' ),
+      config = {
+      /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */      
+        twitter: {
+          consumer_key: process.env.TWITTER_CONSUMER_KEY,
+          consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+          access_token: process.env.TWITTER_ACCESS_TOKEN,
+          access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+        }
+      },
+      T = new Twit( config.twitter );
 
-const path = require("path");
+app.use( express.static( 'public' ) );
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // Set this to true for detailed logging:
-  logger: false,
-});
+let listener = app.listen( process.env.PORT, function(){
+  console.log( 'Your bot is running on port ' + listener.address().port );
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
+  /*
+    Set up a new cron job to start tweeting automatically.
+    Check out https://www.npmjs.com/package/cron#available-cron-patterns to learn more about cron scheduling patterns.
+    
+    For a few examples, see https://glitch.com/edit/#!/creative-bots?path=helpers%2Fcron-schedules.js
 
-// Setup our static files
-fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/", // optional: default '/'
-});
+  */
 
-// Formbody lets us parse incoming forms
-fastify.register(require("@fastify/formbody"));
+  ( new CronJob( '0 */2 * * *', function() {
+    
+    /* The example below tweets out "Hello world 👋" and the current date. */
 
-// View is a templating manager for fastify
-fastify.register(require("@fastify/view"), {
-  engine: {
-    handlebars: require("handlebars"),
-  },
-});
+    const date = new Date().toLocaleString();
 
-// Load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-}
-
-/**
- * Our home page route
- *
- * Returns src/pages/index.hbs with data built into it
- */
-fastify.get("/", function (request, reply) {
-  // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
-
-  // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
-
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo,
-    };
-  }
-
-  // The Handlebars code will be able to access the parameter values and build them into the page
-  return reply.view("/src/pages/index.hbs", params);
-});
-
-/**
- * Our POST route to handle and react to form submissions
- *
- * Accepts body data indicating the user choice
- */
-fastify.post("/", function (request, reply) {
-  // Build the params object to pass to the template
-  let params = { seo: seo };
-
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
-
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-
-    // Load our color data file
-    const colors = require("./src/colors.json");
-
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo,
-      };
-    } else {
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo,
-      };
-    }
-  }
-
-  // The Handlebars template will use the parameter values to update the page with the chosen color
-  return reply.view("/src/pages/index.hbs", params);
-});
-
-// Run the server and report out to the logs
-fastify.listen(
-  { port: process.env.PORT, host: "0.0.0.0" },
-  function (err, address) {
-    if (err) {
-      fastify.log.error(err);
-      process.exit(1);
-    }
-    console.log(`Your app is listening on ${address}`);
-    fastify.log.info(`server listening on ${address}`);
-  }
-);
+    T.post( 'statuses/update', { status: 'Hello world 👋 ' + date }, function( err, data, response ) {
+      if ( err ){
+        console.log( 'error!', err );
+      }
+      else {
+        console.log( 'tweeted', `https://twitter.com/${ data.user.screen_name }/status/${ data.id_str }` );
+      }
+    } );
+  } ) ).start();
+} );
