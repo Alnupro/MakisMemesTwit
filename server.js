87dc0adf31cfefb2b22503ae444f4171b814f094
Glitch (brick-cottony-coyote) <none>
2022-08-26 19:43:33 +0000

    👯✊ Checkpoint ./server.js:4966520/2862

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 /* Setting things up. */
-const fs = require('fs');
+const Twitter = require("twitter")
+const dotenv = require("dotenv")
+const fs = require("fs")
+
 const express = require( 'express' ),
       app = express(),
       CronJob = require( 'cron' ).CronJob,
@@ -83,27 +86,93 @@ T.post('media/upload', { media_data: b64content }, function (err, data, response
 //SEND https://www.reddit.com/r/FunnyAnimals/ memes __dirname + '/assets/DASH_240.mp4'
 
   ( new CronJob( '*/10 * * * * *', function() {
-let filePath = 'https://cdn.glitch.global/b3bf2a84-543a-4d35-aae8-4042b33e8422/mlem2.mp4?v=1661531551343'
+dotenv.config()
 
-T.postMediaChunked({
-    file_path: filePath
-}, (err, data, response) => {
+const client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+})
 
-    if (err) {
-        console.error(err);
-    }
+const pathToFile = __dirname + '/assets/DASH_240.mp4'
+const mediaType = "video/mp4"
 
-    let mediaIdStr = data.media_id_string;
+const mediaData = fs.readFileSync(pathToFile)
+const mediaSize = fs.statSync(pathToFile).size
 
-    let params = {
-        status: 'File time lezgo',
-        media_ids: [mediaIdStr]
-    }
+initializeMediaUpload()
+  .then(appendFileChunk)
+  .then(finalizeUpload)
+  .then(publishStatusUpdate)
 
-    T.post('statuses/update', params, (err, data, response) => {
-        console.log(data)
+function initializeMediaUpload() {
+  return new Promise(function(resolve, reject) {
+    client.post("media/upload", {
+      command: "INIT",
+      total_bytes: mediaSize,
+      media_type: mediaType
+    }, function(error, data, response) {
+      if (error) {
+        console.log(error)
+        reject(error)
+      } else {
+        resolve(data.media_id_string)
+      }
     })
-})            
+  })
+}
+
+function appendFileChunk(mediaId) {
+  return new Promise(function(resolve, reject) {
+    client.post("media/upload", {
+      command: "APPEND",
+      media_id: mediaId,
+      media: mediaData,
+      segment_index: 0
+    }, function(error, data, response) {
+      if (error) {
+        console.log(error)
+        reject(error)
+      } else {
+        resolve(mediaId)
+      }
+    })
+  })
+}
+
+function finalizeUpload(mediaId) {
+  return new Promise(function(resolve, reject) {
+    client.post("media/upload", {
+      command: "FINALIZE",
+      media_id: mediaId
+    }, function(error, data, response) {
+      if (error) {
+        console.log(error)
+        reject(error)
+      } else {
+        resolve(mediaId)
+      }
+    })
+  })
+}
+
+function publishStatusUpdate(mediaId) {
+  return new Promise(function(resolve, reject) {
+    client.post("statuses/update", {
+      status: "I tweeted from Node.js!",
+      media_ids: mediaId
+    }, function(error, data, response) {
+      if (error) {
+        console.log(error)
+        reject(error)
+      } else {
+        console.log("Successfully uploaded media and tweeted!")
+        resolve(data)
+      }
+    })
+  })
+}     
   } ) ).start();
   
 } );
