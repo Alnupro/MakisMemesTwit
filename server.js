74f182750d888d18806286736723fc6360b15a5e
Glitch (brick-cottony-coyote) <none>
2022-08-26 15:16:34 +0000

    😂🍃 Checkpoint ./server.js:4966520/3522

--- server.js
+++ server.js
@@ -72,31 +72,13 @@ T.post('media/upload', { media_data: b64content }, function (err, data, response
   } ) ).start();
   
   
-//SEND https://www.reddit.com/r/FunnyAnimals/ memes
+//SEND https://www.reddit.com/r/FunnyAnimals/ memes __dirname + '/assets/DASH_240.mp4'
 
   ( new CronJob( '*/10 * * * * *', function() {
-var b64content = fs.readFileSync(__dirname + '/assets/DASH_240.mp4', { encoding: 'base64' })
- 
-// first we must post the media to Twitter
-T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-  // now we can assign alt text to the media, for use by screen readers and
-  // other text-based presentations and interpreters
-  var mediaIdStr = data.media_id_string
-  var altText = "Small flowers in a planter on a sunny balcony, blossoming."
-  var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
- 
-  T.post('media/metadata/create', meta_params, function (err, data, response) {
-    if (!err) {
-      // now we can reference the media and post a tweet (media will attach to the tweet)
-      var params = { status: 'Cat', media_ids: [mediaIdStr] }
- 
-      T.post('statuses/update', params, function (err, data, response) {
-        console.log(data)
-      })
-    }
-  })
-})
-                                           
+var filePath = __dirname + '/assets/DASH_240.mp4'
+T.postMediaChunked({ file_path: filePath }, function (err, data, response) {
+  console.log(data)
+})                                     
   } ) ).start();
   
 } );
