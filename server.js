60469a8679f5d8c5e4b08127ed3665725c2a3a68
Alnupro <112330168+Alnupro@users.noreply.github.com>
2022-09-06 21:28:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ downloadFile(VIDEO_URL, 'assets');
      end = new Date().getTime();
   }
 }
-  var old_date = new Date();
+  var old_date;
   var already_vids = [];
   var next_post_url = null
   var save_random_number = null;
